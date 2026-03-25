d0e65ef336e21b8a23d91d436a365a8492a69aeb
jbaker <jbakerdev@gmail.com>
2026-03-25 14:58:09 -0400

    hi

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { useSelector } from 'react-redux';
-import { IconIndex, Log, Modal } from '../enum';
+import { Modal } from '../enum';
 import Viewport from './components/Viewport';
 import NewGame from './components/modals/NewGame';
 import StatusBar from './components/StatusBar';
@@ -17,20 +17,17 @@ import PickNextCard from './components/modals/PickNextCard';
 import DiscardAndDraw from './components/modals/DiscardAndDraw';
 import Winner from './components/modals/Winner';
 import Loser from './components/modals/Loser';
-import { CssIcon } from './common/Shared';
-import { renderEffect } from './components/CardView';
-import { getCardData } from './common/CardUtils';
 import ViewGY from './components/modals/ViewGY';
 import TradeSpells from './components/modals/TradeSpells';
 import CampaignDeckbuilder from './components/CampaignDeckbuilder';
 import MapUI from './components/MapUI';
+import Logs from './components/Logs';
 
 export default () => {
 
   const state = useSelector((state:RState)=>state)
   const netAck = useSelector((state:RState)=>state.netAck)
-  const match = useSelector((state:RState)=>state.saveFile?.currentMatch)
-
+  
   const getModal = () => {
     switch(state.activeModal){
       case Modal.NewGame: return <NewGame/>
@@ -58,9 +55,7 @@ export default () => {
       {state.activeModal && <div style={{position:'absolute', height:'fit-content', width:'800px', left:0,right:0,bottom:0,top:0, margin:'auto', zIndex:1}}>{getModal()}</div>}
       {!netAck && <div style={{position:'absolute', top:0, left:0, width:'100vw', height:'100vh', background:'white', opacity:0.1, zIndex:2}}/>}
       <div style={{position:'relative'}}>
-        {showUI && <div style={{width:'200px', opacity:0.8, position:'absolute', top:'33%', left:10, height:'300px', overflow:'auto', background:'black', padding:'5px', fontSize:'16px'}}>
-            {match?.logs?.map(l=>getLogEl(l, match))}
-        </div>}
+        {showUI && <Logs/>}
         {showUI && <StatusBar />}
         {showUI && <CPUDeck/>}
         <Viewport/>
@@ -71,30 +66,4 @@ export default () => {
       </div>
     </div>
   )
-}
-
-
-const getLogEl = (l:LogEntry, match:MatchState) => {
-
-  const caster = match.players.find(p=>p.id === l.card.ownerId).playerSprite
-  const dat = getCardData(l.card)
-
-  if(l.kind === Log.AbilityPlayed) return <div>
-      <CssIcon spriteIndex={caster}/> used ability of {l.card.kind} : {renderEffect(dat.ability.effect)}
-  </div>
-  if(l.kind === Log.CardPlayed) return <div>
-      <CssIcon spriteIndex={caster}/> summoned {l.card.kind}
-  </div>
-  if(l.kind === Log.RangedDamage) return <div>
-      <CssIcon spriteIndex={caster}/> used ranged attack of {l.card.kind} on {l.target.kind}
-  </div>
-  if(l.kind === Log.ExpiredEffect) return <div>
-      <CssIcon spriteIndex={IconIndex.Debuff}/> Effect expired on {l.card.kind}: {renderEffect(l.effect.status)}
-  </div>
-  if(l.kind === Log.NimbleActivation) return <div>
-    <CssIcon spriteIndex={dat.sprite}/> was moved
-  </div>
-  if(l.kind === Log.Destroyed) return <div>
-    <CssIcon spriteIndex={dat.sprite}/> was destroyed
-  </div>
 }
---
+++ src/components/Logs.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import { useSelector } from 'react-redux';
+import { CssIcon } from '../common/Shared';
+import { IconIndex, Log } from '../../enum';
+import { getCardData } from '../common/CardUtils';
+import { renderEffect } from './CardView';
+
+export default () => {
+    const match = useSelector((state:RState)=>state.saveFile?.currentMatch)
+    const bottomRef = React.useRef(null);
+
+    React.useEffect(() => {
+        const el = bottomRef.current;
+        if (el) {
+            el.scrollTop = el.scrollHeight;
+        }
+    }, [match.logs]);
+
+    return (
+        <div ref={bottomRef} style={{width:'200px', opacity:0.8, position:'absolute', top:'33%', left:10, height:'300px', overflow:'auto', background:'black', padding:'5px', fontSize:'16px'}}>
+            {match.logs.map(l=>getLogEl(l, match))}
+        </div>
+    )
+}
+
+const getLogEl = (l:LogEntry, match:MatchState) => {
+
+    const caster = match.players.find(p=>p.id === l.card.ownerId).playerSprite
+    const dat = getCardData(l.card)
+  
+    if(l.kind === Log.AbilityPlayed) return <div>
+        <CssIcon spriteIndex={caster}/> used ability of {l.card.kind} : {renderEffect(dat.ability.effect)}
+    </div>
+    if(l.kind === Log.CardPlayed) return <div>
+        <CssIcon spriteIndex={caster}/> summoned {l.card.kind}
+    </div>
+    if(l.kind === Log.RangedDamage) return <div>
+        <CssIcon spriteIndex={caster}/> used ranged attack of {l.card.kind} on {l.target.kind}
+    </div>
+    if(l.kind === Log.ExpiredEffect) return <div>
+        <CssIcon spriteIndex={IconIndex.Debuff}/> Effect expired on {l.card.kind}: {renderEffect(l.effect.status)}
+    </div>
+    if(l.kind === Log.NimbleActivation) return <div>
+      <CssIcon spriteIndex={dat.sprite}/> was moved
+    </div>
+    if(l.kind === Log.Destroyed) return <div>
+      <CssIcon spriteIndex={dat.sprite}/> was destroyed
+    </div>
+  }
+  
